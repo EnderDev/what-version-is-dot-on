ad2268686e79cc49a1fbfcd6af956a92057a2f42
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-30 20:55:42 +0100

    ✨ Add metatags for Discordbot to enjoy

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ invalidate(true);
 app.all("/", async (req, res) => {
     invalidate();
 
+    if(req.headers["user-agent"].toLowerCase().includes("discord")) {
+        res.send(`
+<meta property="og:title" content="What version is Dot Browser on?">
+<meta name="twitter:title" content="What version is Dot Browser on?">
+<meta name="theme-color" content="#2F3136">
+<meta name="og:image" content="">
+<meta name="twitter:image" content="">
+<meta property="og:description" content="${cached}">
+<meta name="description" content="${cached}">
+<meta property="twitter:description" content="${cached}">
+        `)
+    }
+
     res.end(cached)
 })
 
