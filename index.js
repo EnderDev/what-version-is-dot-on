a49ee7ef3277bbc22cc042a5cd08c042582ca568
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-30 20:44:42 +0100

    🔥 Remove cache

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const axios = require("axios");
 
 const app = express();
 
-const cache = {}
-
 app.all("/", async (req, res) => {
     res.end(
         await axios.get("https://raw.githubusercontent.com/dothq/browser-desktop/nightly/package.json")
