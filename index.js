c5a74b83fb10345f672c330cac7297d6217a7e65
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-30 21:09:51 +0100

    ✨ Use HTML for all requests

--- index.js
+++ index.js
@@ -20,23 +20,21 @@ invalidate(true);
 app.all("/", async (req, res) => {
     invalidate();
 
-    const device = deviceDetector.parse(req.headers["user-agent"]);
-
-    if(device.bot && device.bot.name && device.bot.url) {
-        res.header("content-type", "text/html").send(`
-<meta property="og:title" content="What version is Dot Browser on?">
-<meta name="twitter:title" content="What version is Dot Browser on?">
-<meta name="theme-color" content="#2F3136">
-<meta name="og:image" content="">
-<meta name="twitter:image" content="">
-<meta property="og:description" content="${cached}">
-<meta name="description" content="${cached}">
-<meta property="twitter:description" content="${cached}">
-${cached}
-        `)
-    } else {
-        res.end(cached)
-    }
+    res.send(`
+<html>
+    <head>
+        <meta property="og:title" content="What version is Dot Browser on?">
+        <meta name="twitter:title" content="What version is Dot Browser on?">
+        <meta name="theme-color" content="#2F3136">
+        <meta name="og:image" content="">
+        <meta name="twitter:image" content="">
+        <meta property="og:description" content="${cached}">
+        <meta name="description" content="${cached}">
+        <meta property="twitter:description" content="${cached}">
+    </head>
+    <body>${cached}</body>
+</html>
+    `)
 })
 
 app.all("/badge", async (req, res) => {
